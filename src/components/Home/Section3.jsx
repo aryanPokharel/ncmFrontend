679cdc2673e3d8b14b8cdad87cf0fca4ef86c9d4
Aryan Pokharel <64863058+aryanPokharel@users.noreply.github.com>
2023-04-05 17:46:54 +0545

    Update Section3.jsx

--- src/components/Home/Section3.jsx
+++ src/components/Home/Section3.jsx
@@ -23,15 +23,6 @@ const StoryCard = (props) => {
         </Typography>
       </CardContent>   
     </Card>
-    // <div className="card" id='section3Card'>
-    //   <div id='storyCardImgSection'>
-    //     <img src={props.imageUrl} className="card-img-top"/>
-    //   </div>
-    //   <div className="card-body">
-    //     <h5 className="card-title">{props.title}</h5>
-    //     <p className="card-text">{props.content}</p>
-    //   </div>
-    // </div>
   )
 }
 
