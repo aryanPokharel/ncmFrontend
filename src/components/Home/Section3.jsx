6af0aeac3076206f35226ec61d4ad2d076c4f83c
Aryan Pokharel <64863058+aryanPokharel@users.noreply.github.com>
2023-04-05 17:56:26 +0545

    Section 3 fixed

--- src/components/Home/Section3.jsx
+++ src/components/Home/Section3.jsx
@@ -2,27 +2,27 @@ import Button from '@mui/material/Button';
 import './Section3.css'
 import * as React from 'react';
 import Card from '@mui/material/Card';
-import CardActions from '@mui/material/CardActions';
 import CardContent from '@mui/material/CardContent';
 import CardMedia from '@mui/material/CardMedia';
 import Typography from '@mui/material/Typography';
 const StoryCard = (props) => {
   return (
-    <Card sx={{ maxWidth: 345 }}>
-      <CardMedia
-        sx={{ height: 140 }}
-        image={props.imageUrl}
-        title="green iguana"
-      />
-      <CardContent>
-        <Typography gutterBottom variant="h5" component="div">
-        {props.title}
-        </Typography>
-        <Typography variant="body2" color="text.secondary">
-        {props.content}
-        </Typography>
-      </CardContent>   
-    </Card>
+    <Card sx={{ maxWidth: 400 }} id='section3Card'>
+  {/* <CardMedia
+    sx={{ height: 200, objectFit: 'cover' }} 
+    image={props.imageUrl}
+  /> */}
+  <img src={props.imageUrl} width={"100%" } height={"240vh"} />
+  <CardContent>
+    <Typography gutterBottom variant="h5" component="div">
+      {props.title}
+    </Typography>
+    <Typography variant="body2" color="text.secondary">
+      {props.content}
+    </Typography>
+  </CardContent>   
+</Card>
+
   )
 }
 
@@ -39,7 +39,7 @@ const Section3 = () => {
           content={"The logistics bottleneck is because of poor and unprofessional transport infrastructure. We started owning and managing our own fleet to provide secure & timely service."} />
 
         <StoryCard imageUrl={"https://i0.wp.com/www.nepalcanmove.com/wp-content/uploads/2022/01/DSC00332-1-Square-Walking-Bridge-R-2.jpg?w=512&ssl=1"}
-          title={"Give us the challenge we will take it"}
+          title={"Give us the challenge!"}
           content={"We have expanded our services to Karnali and far west, often delivering on foot, human back and donkeys. It is a challenge, but we are here to provide services to Humla and Pyuthan as much as Dharan and Birtamod."} />
       </div>
       <div id='section3BtnSection'>
