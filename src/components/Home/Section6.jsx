571a0dbca6334e07bd89c74dd59768d4d7dc97b7
Aryan Pokharel <64863058+aryanPokharel@users.noreply.github.com>
2023-04-05 15:46:22 +0545

    Accordion update

--- src/components/Home/Section6.jsx
+++ src/components/Home/Section6.jsx
@@ -4,98 +4,82 @@ import AccordionDetails from '@mui/material/AccordionDetails';
 import AccordionSummary from '@mui/material/AccordionSummary';
 import Typography from '@mui/material/Typography';
 import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
+import './Section6.css'
 
-const Section6 = ()=> {
-  const [expanded, setExpanded] = React.useState(false);
+const Section6 = () => {
+    const [expanded, setExpanded] = React.useState(false);
 
-  const handleChange = (panel) => (event, isExpanded) => {
-    setExpanded(isExpanded ? panel : false);
-  };
+    const handleChange = (panel) => (event, isExpanded) => {
+        setExpanded(isExpanded ? panel : false);
+    };
 
-  return (
-    <div>
-      <Accordion expanded={expanded === 'panel1'} onChange={handleChange('panel1')}>
-        <AccordionSummary
-          expandIcon={<ExpandMoreIcon />}
-          aria-controls="panel1bh-content"
-          id="panel1bh-header"
-        >
-          <Typography sx={{ width: '33%', flexShrink: 0 }}>
-          Manisha K.C.
-          </Typography>
-          <Typography sx={{ color: 'text.secondary' }}>CEO, MAHABOUDDHA WHOLESALE.</Typography>
-        </AccordionSummary>
-        <AccordionDetails>
-          <Typography>
-          <h2>"Most supportive team!"</h2>
-            <p>
-            "Being in a e-commerce business for almost 10 years, 
-we were always looking for delivery partner who can deliver all over Nepal. 
-As we started working together with NCM, 
-we can almost deliver all over Nepal with cash on delivery service at very best rate. 
-Nepal Can Move with the most supportive team they took online business to the next level. 
-We wish Nepal Can Move reach to the next level and make online shopping more easy and 
-more affordable."
-            </p>
-          </Typography>
-        </AccordionDetails>
-      </Accordion>
-      <Accordion expanded={expanded === 'panel2'} onChange={handleChange('panel2')}>
-        <AccordionSummary
-          expandIcon={<ExpandMoreIcon />}
-          aria-controls="panel2bh-content"
-          id="panel2bh-header"
-        >
-          <Typography sx={{ width: '33%', flexShrink: 0 }}>Users</Typography>
-          <Typography sx={{ color: 'text.secondary' }}>
-            You are currently not an owner
-          </Typography>
-        </AccordionSummary>
-        <AccordionDetails>
-          <Typography>
-            Donec placerat, lectus sed mattis semper, neque lectus feugiat lectus,
-            varius pulvinar diam eros in elit. Pellentesque convallis laoreet
-            laoreet.
-          </Typography>
-        </AccordionDetails>
-      </Accordion>
-      <Accordion expanded={expanded === 'panel3'} onChange={handleChange('panel3')}>
-        <AccordionSummary
-          expandIcon={<ExpandMoreIcon />}
-          aria-controls="panel3bh-content"
-          id="panel3bh-header"
-        >
-          <Typography sx={{ width: '33%', flexShrink: 0 }}>
-            Advanced settings
-          </Typography>
-          <Typography sx={{ color: 'text.secondary' }}>
-            Filtering has been entirely disabled for whole web server
-          </Typography>
-        </AccordionSummary>
-        <AccordionDetails>
-          <Typography>
-            Nunc vitae orci ultricies, auctor nunc in, volutpat nisl. Integer sit
-            amet egestas eros, vitae egestas augue. Duis vel est augue.
-          </Typography>
-        </AccordionDetails>
-      </Accordion>
-      <Accordion expanded={expanded === 'panel4'} onChange={handleChange('panel4')}>
-        <AccordionSummary
-          expandIcon={<ExpandMoreIcon />}
-          aria-controls="panel4bh-content"
-          id="panel4bh-header"
-        >
-          <Typography sx={{ width: '33%', flexShrink: 0 }}>Personal data</Typography>
-        </AccordionSummary>
-        <AccordionDetails>
-          <Typography>
-            Nunc vitae orci ultricies, auctor nunc in, volutpat nisl. Integer sit
-            amet egestas eros, vitae egestas augue. Duis vel est augue.
-          </Typography>
-        </AccordionDetails>
-      </Accordion>
-    </div>
-  );
+    return (
+        <div id='accordionHolder'>
+            <Accordion className='accordions' expanded={expanded === 'panel1'} onChange={handleChange('panel1')}>
+                <AccordionSummary
+                    expandIcon={<ExpandMoreIcon />}
+                    aria-controls="panel1bh-content"
+                    id="panel1bh-header"
+                >
+                    <Typography sx={{ width: '33%', flexShrink: 0 }}>
+                        Manisha K.C.
+                    </Typography>
+                    <Typography sx={{ color: 'text.secondary' }}>CEO, MAHABOUDDHA WHOLESALE.</Typography>
+                </AccordionSummary>
+                <AccordionDetails>
+                    <Typography>
+                        <h2>"Most supportive team!"</h2>
+                        <p>
+                            "Being in a e-commerce business for almost 10 years,
+                            we were always looking for delivery partner who can deliver all over Nepal.
+                            As we started working together with NCM,
+                            we can almost deliver all over Nepal with cash on delivery service at very best rate.
+                            Nepal Can Move with the most supportive team they took online business to the next level.
+                            We wish Nepal Can Move reach to the next level and make online shopping more easy and
+                            more affordable."
+                        </p>
+                    </Typography>
+                </AccordionDetails>
+            </Accordion>
+            <Accordion  className='accordions' expanded={expanded === 'panel2'} onChange={handleChange('panel2')}>
+                <AccordionSummary
+                    expandIcon={<ExpandMoreIcon />}
+                    aria-controls="panel2bh-content"
+                    id="panel2bh-header"
+                >
+                    <Typography sx={{ width: '33%', flexShrink: 0 }}>John Doe</Typography>
+                    <Typography sx={{ color: 'text.secondary' }}>
+                        Enterpreneur
+                    </Typography>
+                </AccordionSummary>
+                <AccordionDetails>
+                    <Typography>
+                        NCM has helped me run my business smoothly for the last 5 years!
+                    </Typography>
+                </AccordionDetails>
+            </Accordion>
+            <Accordion  className='accordions' expanded={expanded === 'panel3'} onChange={handleChange('panel3')}>
+                <AccordionSummary
+                    expandIcon={<ExpandMoreIcon />}
+                    aria-controls="panel3bh-content"
+                    id="panel3bh-header"
+                >
+                    <Typography sx={{ width: '33%', flexShrink: 0 }}>
+                        Ram Bahadur
+                    </Typography>
+                    <Typography sx={{ color: 'text.secondary' }}>
+                        Shopping Enthusiast
+                    </Typography>
+                </AccordionSummary>
+                <AccordionDetails>
+                    <Typography>
+                        As a person who loves shopping, i find NCM very much efficient in moving goods across the country within least possible time frame!
+                    </Typography>
+                </AccordionDetails>
+            </Accordion>
+            
+        </div>
+    );
 }
 
 export default Section6;
