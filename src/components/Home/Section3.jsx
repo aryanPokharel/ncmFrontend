2d328dd5141d134635acabd4ae6c3d1bf420b4e2
Aryan Pokharel <64863058+aryanPokharel@users.noreply.github.com>
2023-04-06 10:51:36 +0545

    Update Section3.jsx

--- src/components/Home/Section3.jsx
+++ src/components/Home/Section3.jsx
@@ -8,22 +8,22 @@ import Typography from '@mui/material/Typography';
 const StoryCard = (props) => {
   return (
     <Card sx={{ maxWidth: 400 }} id='section3Card'>
-  {/* <CardMedia
+      {/* <CardMedia
     sx={{ height: 200, objectFit: 'cover' }} 
     image={props.imageUrl}
   /> */}
-  <div id='imageSection'>
-  <img src={props.imageUrl} width={"100%" } height={"240vh"} />
-  </div>
-  <CardContent>
-    <Typography gutterBottom variant="h5" component="div">
-      {props.title}
-    </Typography>
-    <Typography variant="body2" color="text.secondary" id="cardContent">
-      {props.content}
-    </Typography>
-  </CardContent>   
-</Card>
+      <div id='imageSection'>
+        <img src={props.imageUrl} width={"100%"} height={"240vh"} />
+      </div>
+      <CardContent>
+        <Typography gutterBottom variant="h5" component="div">
+          {props.title}
+        </Typography>
+        <Typography variant="body2" color="text.secondary" id="cardContent">
+          {props.content}
+        </Typography>
+      </CardContent>
+    </Card>
 
   )
 }
@@ -31,8 +31,8 @@ const StoryCard = (props) => {
 const Section3 = () => {
   return (
     <section id='section3'>
-     
-      <div id='section3StorySection'>
+
+      {/* <div id='section3StorySection'>
         <StoryCard imageUrl={"https://i0.wp.com/www.nepalcanmove.com/wp-content/uploads/2022/01/DSC00276-Square-NCM-Delivery-R-2.1.jpg?w=511&ssl=1"}
           title={"Villages as important as towns"}
           content={"While many focus on big cities we have focused on expanding our services all over Nepal. Our experience shows it is the villages and small towns that need ecommerce more."} />
@@ -46,6 +46,26 @@ const Section3 = () => {
       </div>
       <div id='section3BtnSection'>
         <Button variant="contained" id='track-btn'>View More</Button>
+      </div> */}
+
+      <div class="container">
+        <div class="row">
+          <div class="col-sm">
+          <StoryCard imageUrl={"https://i0.wp.com/www.nepalcanmove.com/wp-content/uploads/2022/01/DSC00276-Square-NCM-Delivery-R-2.1.jpg?w=511&ssl=1"}
+          title={"Villages as important as towns"}
+          content={"While many focus on big cities we have focused on expanding our services all over Nepal. Our experience shows it is the villages and small towns that need ecommerce more."} />
+          </div>
+          <div class="col-sm">
+          <StoryCard imageUrl={"https://cdn.techjockey.com/blog/wp-content/uploads/2018/12/13172658/Top-10-Courier-Management-Software-in-India-2022_feature.jpg"}
+          title={"Transport is the critical factor"}
+          content={"The logistics bottleneck is because of poor and unprofessional transport infrastructure. We started owning and managing our own fleet to provide secure & timely service."} />
+          </div>
+          <div class="col-sm">
+          <StoryCard imageUrl={"https://i0.wp.com/www.nepalcanmove.com/wp-content/uploads/2022/01/DSC00332-1-Square-Walking-Bridge-R-2.jpg?w=512&ssl=1"}
+          title={"Give us the challenge!"}
+          content={"We have expanded our services to Karnali and far west, often delivering on foot, human back and donkeys. It is a challenge, but we are here to provide services to Humla and Pyuthan as much as Dharan and Birtamod."} />
+          </div>
+        </div>
       </div>
     </section>
   )
