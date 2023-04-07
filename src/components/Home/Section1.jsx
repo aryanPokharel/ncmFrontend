8d312da014a86077bce83f01ba0ed4ad147ffa5d
Aryan Pokharel <64863058+aryanPokharel@users.noreply.github.com>
2023-04-08 00:38:03 +0545

    Section1 cleanup

--- src/components/Home/Section1.jsx
+++ src/components/Home/Section1.jsx
@@ -11,23 +11,7 @@ const Section1 = () => {
         <Button variant="contained" id='track-btn'>GET STARTED</Button>
       </div>
     </section>
-    // <div className="row" id='section1'>
-    //   <div className="col-12">
-    //   </div>
-    //   <div className="col-10">
-    //     <div className="container">
-    //       <div className="row">
-    //         <div className="col-md-6 my-auto" id='section1ContentSection'>
-
-    // <h3 id='contentItem'>Sell more online with Nepal Can Move</h3>
-    // <h1 id='contentItem'>Nepal's national courier, cargo, delivery and logistics company</h1>
-    // <p id='contentItem'>Nepal Can Move, your full-service courier & logistic partner, Fast. Secure. Reliable.  You Can Count On us for Your Success and Growth. Experience service like never before.</p>
-    // <Button variant="contained" id='track-btn'>GET STARTED</Button>
-    //         </div>
-    //       </div>
-    //     </div>
-    //   </div>
-    // </div>
+    
   )
 }
 
