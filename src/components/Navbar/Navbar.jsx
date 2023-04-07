4ad6cbbe6791a4267abc3e3e3359fff260426813
Aryan Pokharel <64863058+aryanPokharel@users.noreply.github.com>
2023-04-07 22:22:21 +0545

    navbar updates

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -3,72 +3,97 @@ import './Navbar.css';
 import Button from '@mui/material/Button';
 
 const Navbar = () => {
-  const [isOpen, setIsOpen] = useState(false);
 
   const [isLoggedIn, setIsLoggedIn] = useState(true);
   return (
-        <nav className="navbar navbar-expand-lg navbar-dark bg-transparent">
+    // <nav>
+    //   <input type="checkbox" id="check" />
+    //   <label htmlFor="check" className="checkbtn">
+    //     <i className="fas fa-bars" />
+    //   </label>
+    //   <label className="logo">DesignX</label>
+    //   <ul>
+    //     <li>
+    //       <a className="active" href="#">
+    //         Home
+    //       </a>
+    //     </li>
+    //     <li>
+    //       <a href="#">About</a>
+    //     </li>
+    //     <li>
+    //       <a href="#">Services</a>
+    //     </li>
+    //     <li>
+    //       <a href="#">Contact</a>
+    //     </li>
+    //     <li>
+    //       <a href="#">Feedback</a>
+    //     </li>
+    //   </ul>
+    // </nav>
+    <nav className="navbar navbar-expand-lg navbar-dark bg-transparent">
 
-          <div className="container-fluid" id='navbarContainer'>
+      <div className="container-fluid" id='navbarContainer'>
 
-            <button
-              className="navbar-toggler"
-              type="button"
-              data-mdb-toggle="collapse"
-              data-mdb-target="#navbarSupportedContent"
-              aria-controls="navbarSupportedContent"
-              aria-expanded="false"
-              aria-label="Toggle navigation"
-            >
-              <i className="fas fa-bars" />
-            </button>
+        <button
+          className="navbar-toggler"
+          type="button"
+          data-mdb-toggle="collapse"
+          data-mdb-target="#navbarSupportedContent"
+          aria-controls="navbarSupportedContent"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
+        >
+          <i className="fas fa-bars" />
+        </button>
 
-            <div className="collapse navbar-collapse" id="navbarSupportedContent">
+        <div className="collapse navbar-collapse" id="navbarSupportedContent">
 
-              <a className="navbar-brand mt-2 mt-lg-0" href="#">
-                <img
-                  src="data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAOEAAADhCAMAAAAJbSJIAAAAdVBMVEXdHj/////cK0fcJkPdHT3bMU/dFDnhXXPhaXzcAC/aMErcADPcADHcCDXsrrnonqXvusLdQVfmj5jqo6/pnqr11dnzys7dUmf03N7kg4znpavcQlvrqLTu0tXvzdLtwsnjcoPbACLbACjbACHhd4bvusPjk56mka92AAACCElEQVR4nO3c23ISURBAURQykQQ1MSqo5Krx/z9RX3iwCk+PBLo5uPYH9MyqqXk6l8nryWk3nRD2HmH/EfYfYf8R9h9h/xH2H2H/EfYfYf8Rtpot5lGLYY/vulsvEA7L1aeo1Zdy4kuE56/i3l/u8213ibARIWFShI0ICZMibERImBRhI0LCpAgbERImRdiIkDApwkaEhEkRNiIkTOrgwvksp2GY1Qi/nl0n9e1iO/HQwsTWi1MX3mz/5U9J6BsSEpZHSEhYHyEhYX2EhIT1ERIS1kdISFgfISFhfYSEhPWta4S363dhdyPm3MdjHrYfjD/4XozHRdTjhxFzzp/COX85+X8E+2nejhG+2fnmAsJGhISbCAmDCBsREm4iJAwibERIuImQMIiwESHhJkLCIMJGhISbCAmDCBv1LhzCG+WflmOE38M5o9aA9y8cflw9XwWNWX9fxWOeb0uElzcjHpvYAYQfq01/RkhIWB8hIWF9hISE9RESEtZHSEhYHyEhYX2EhIT1ERIS1kf478LZ9CKr6c8S4W9jVouj3U+zr453x9C+IiQMIkyIkDCIMCFCwiDChAgJgwgTIiQMIkyIkDCIMCFCwiDChAgJgwgTGh7uPkfdLXsWjrjcYD6f7Tz9GISHjbD/CPuPsP8I+4+w/wj7j7D/CPuPsP/+B+E07WBFTZPJ9dmJ9wtiZ5EFKqzj8AAAAABJRU5ErkJggg=="
-                  height={35}
-                  alt="MDB Logo"
-                  loading="lazy"
-                />
-              </a>
+          <a className="navbar-brand mt-2 mt-lg-0" href="#">
+            <img
+              src="data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAOEAAADhCAMAAAAJbSJIAAAAdVBMVEXdHj/////cK0fcJkPdHT3bMU/dFDnhXXPhaXzcAC/aMErcADPcADHcCDXsrrnonqXvusLdQVfmj5jqo6/pnqr11dnzys7dUmf03N7kg4znpavcQlvrqLTu0tXvzdLtwsnjcoPbACLbACjbACHhd4bvusPjk56mka92AAACCElEQVR4nO3c23ISURBAURQykQQ1MSqo5Krx/z9RX3iwCk+PBLo5uPYH9MyqqXk6l8nryWk3nRD2HmH/EfYfYf8R9h9h/xH2H2H/EfYfYf8Rtpot5lGLYY/vulsvEA7L1aeo1Zdy4kuE56/i3l/u8213ibARIWFShI0ICZMibERImBRhI0LCpAgbERImRdiIkDApwkaEhEkRNiIkTOrgwvksp2GY1Qi/nl0n9e1iO/HQwsTWi1MX3mz/5U9J6BsSEpZHSEhYHyEhYX2EhIT1ERIS1kdISFgfISFhfYSEhPWta4S363dhdyPm3MdjHrYfjD/4XozHRdTjhxFzzp/COX85+X8E+2nejhG+2fnmAsJGhISbCAmDCBsREm4iJAwibERIuImQMIiwESHhJkLCIMJGhISbCAmDCBv1LhzCG+WflmOE38M5o9aA9y8cflw9XwWNWX9fxWOeb0uElzcjHpvYAYQfq01/RkhIWB8hIWF9hISE9RESEtZHSEhYHyEhYX2EhIT1ERIS1kf478LZ9CKr6c8S4W9jVouj3U+zr453x9C+IiQMIkyIkDCIMCFCwiDChAgJgwgTIiQMIkyIkDCIMCFCwiDChAgJgwgTGh7uPkfdLXsWjrjcYD6f7Tz9GISHjbD/CPuPsP8I+4+w/wj7j7D/CPuPsP/+B+E07WBFTZPJ9dmJ9wtiZ5EFKqzj8AAAAABJRU5ErkJggg=="
+              height={35}
+              alt="MDB Logo"
+              loading="lazy"
+            />
+          </a>
 
-              <ul className="navbar-nav me-auto mb-2 mb-lg-0" id='linkSection'>
-                <li className="nav-item">
-                  <a href="/" className="navbar-link">Home</a>
-                </li>
-                <li className="nav-item">
-                  <a href="/about" className="navbar-link">About</a>
-                </li>
-                <li className="nav-item">
-                  <a href="/services" className="navbar-link">Services</a>
-                </li>
+          <ul className="navbar-nav me-auto mb-2 mb-lg-0" id='linkSection'>
+            <li className="nav-item">
+              <a href="/" className="navbar-link">Home</a>
+            </li>
+            <li className="nav-item">
+              <a href="/about" className="navbar-link">About</a>
+            </li>
+            <li className="nav-item">
+              <a href="/services" className="navbar-link">Services</a>
+            </li>
 
-                <li className="nav-item">
-                  <a href="/contact" className="navbar-link">Contact</a>
-                </li>
-                <li className="nav-item">
-                  <a href="/newsMedia" className="navbar-link">News & Media</a>
-                </li>
-                <li className="nav-item">
-                  <a href="/locations" className="navbar-link">Locations</a>
-                </li>
+            <li className="nav-item">
+              <a href="/contact" className="navbar-link">Contact</a>
+            </li>
+            <li className="nav-item">
+              <a href="/newsMedia" className="navbar-link">News & Media</a>
+            </li>
+            <li className="nav-item">
+              <a href="/locations" className="navbar-link">Locations</a>
+            </li>
 
-                <li className="nav-item">
-                  <a href="/about" className="navbar-link">Jobs</a>
-                </li>
+            <li className="nav-item">
+              <a href="/about" className="navbar-link">Jobs</a>
+            </li>
 
-                <li className="nav-item">
-                  <Button variant="contained" id='track-btn'>Track</Button>
-                </li>
-              </ul>
+            <li className="nav-item">
+              <Button variant="contained" id='track-btn'>Track</Button>
+            </li>
+          </ul>
 
-            </div>
+        </div>
 
-            {
-              isLoggedIn ? 
-              <div className="d-flex align-items-center">
+        {
+          isLoggedIn ?
+            <div className="d-flex align-items-center">
               <div className="dropdown">
                 <a
                   className="dropdown-toggle d-flex align-items-center hidden-arrow"
@@ -109,11 +134,11 @@ const Navbar = () => {
               </div>
             </div> :
             <Button variant="contained" id='login-btn'>Login</Button>
-            }
+        }
 
-          </div>
+      </div>
 
-        </nav>
+    </nav>
 
   );
 };
