d42590aec7247e180a31631c09a814205be8e4c4
Aryan Pokharel <64863058+aryanPokharel@users.noreply.github.com>
2023-04-07 10:07:01 +0545

    Update CardDeck.jsx

--- src/components/AboutUs/CardDeck.jsx
+++ src/components/AboutUs/CardDeck.jsx
@@ -1,52 +1,120 @@
-import React from 'react'
+import * as React from 'react';
+import { experimentalStyled as styled } from '@mui/material/styles';
+import Box from '@mui/material/Box';
+import Paper from '@mui/material/Paper';
+import Grid from '@mui/material/Grid';
+import Card from '@mui/material/Card';
+import CardActions from '@mui/material/CardActions';
+import CardContent from '@mui/material/CardContent';
+import Button from '@mui/material/Button';
+import Typography from '@mui/material/Typography';
+
+const Item = styled(Paper)(({ theme }) => ({
+    backgroundColor: theme.palette.mode === 'dark' ? '#1A2027' : '#fff',
+    ...theme.typography.body2,
+    padding: theme.spacing(2),
+    textAlign: 'center',
+    color: theme.palette.text.secondary,
+}));
+
+const cardData = [
+    {
+        index : 0,
+        title: "Discover",
+        content: "Business is all about solving problems. We start with discovering the logistics problems and pain-points."
+    },
+    {
+        index : 1,
+        title: "Define",
+        content: "We define what would be the optimal solution to the problems that want solutions for."
+    },
+    {
+        index : 2,
+        title: "Design",
+        content: "We carefully design the systems that would achieve the defined solution to the problems."
+    },
+    {
+        index : 3,
+        title: "Develop",
+        content: "We develop the designed systems. We use latest technology to develop the systems and solutions."
+    },
+    {
+        index : 4,
+        title: "Deploy",
+        content: "We deploy the developed solution. We test, improve, deploy and repeat until the defined standards are acheived."
+    },
+    {
+        index : 5,
+        title: "Deliver",
+        content: "We deliver solution to the client and continue the 6-D process on a periodic ongoing basis."
+    }
+]
+
+const ReturnCardInfo = (info) => {
+    return (
+        <Card sx={{ minWidth: 275 }}>
+            
+            <CardContent>
+                <Typography sx={{ fontSize: 14 }} color="text.secondary" gutterBottom>
+                    {info.cardNumber+1}
+                </Typography>
+                <Typography variant="h5" component="div">
+                {cardData[info.cardNumber].title}
+                </Typography>
+                <Typography sx={{ mb: 1.5 }} color="text.secondary">
+                    
+                </Typography>
+                <Typography variant="body2">
+                {cardData[info.cardNumber].content}
+                    <br />
+                    
+                </Typography>
+            </CardContent>
+            <CardActions>
+                <Button size="small">Learn More</Button>
+            </CardActions>
+        </Card>
+        // (cardIndex = 0) ?
+        // {
+        //     title: "Discover",
+        //     content: "Business is all about solving problems. We start with discovering the logistics problems and pain-points."
+        // } :
+        //     (cardIndex = 1) ?
+        // {
+        //     title: "Define",
+        //     content: "We define what would be the optimal solution to the problems that want solutions for."
+        // } :
+        //         (cardIndex = 2) ?
+        // {
+        //     title: "Design",
+        //     content: "We carefully design the systems that would achieve the defined solution to the problems."
+        // } :
+        // (cardIndex = 3) ? {
+        //     title: "Develop",
+        //     content: "We develop the designed systems. We use latest technology to develop the systems and solutions."
+        // } :
+        //                 (cardIndex = 4) ? {
+        //                     title: "Deploy",
+        //                     content: "We deploy the developed solution. We test, improve, deploy and repeat until the defined standards are acheived."
+        //                 } :
+        // {
+        //     title: "Deliver",
+        //     content: "We deliver solution to the client and continue the 6-D process on a periodic ongoing basis."
+        // }
+    )
+}
 
 const CardDeck = () => {
     return (
-        <div>
-            <div className="card-deck">
-                <div className="card">
-                    <img className="card-img-top" src=".../100px180/" alt="Card image cap" />
-                    <div className="card-body">
-                        <h5 className="card-title">Card title</h5>
-                        <p className="card-text">
-                            This is a wider card with supporting text below as a natural lead-in to
-                            additional content. This content is a little bit longer.
-                        </p>
-                    </div>
-                    <div className="card-footer">
-                        <small className="text-muted">Last updated 3 mins ago</small>
-                    </div>
-                </div>
-                <div className="card">
-                    <img className="card-img-top" src=".../100px180/" alt="Card image cap" />
-                    <div className="card-body">
-                        <h5 className="card-title">Card title</h5>
-                        <p className="card-text">
-                            This card has supporting text below as a natural lead-in to additional
-                            content.
-                        </p>
-                    </div>
-                    <div className="card-footer">
-                        <small className="text-muted">Last updated 3 mins ago</small>
-                    </div>
-                </div>
-                <div className="card">
-                    <img className="card-img-top" src=".../100px180/" alt="Card image cap" />
-                    <div className="card-body">
-                        <h5 className="card-title">Card title</h5>
-                        <p className="card-text">
-                            This is a wider card with supporting text below as a natural lead-in to
-                            additional content. This card has even longer content than the first to
-                            show that equal height action.
-                        </p>
-                    </div>
-                    <div className="card-footer">
-                        <small className="text-muted">Last updated 3 mins ago</small>
-                    </div>
-                </div>
-            </div>
-
-        </div>
+        <Box sx={{ flexGrow: 1 }}>
+            <Grid container spacing={2} >
+                {Array.from(Array(6)).map((_, index) => (
+                    <Grid item xs={2} sm={4} md={4} key={index}>
+                        <Item><ReturnCardInfo cardNumber={index} /></Item>
+                    </Grid>
+                ))}
+            </Grid>
+        </Box>
     )
 }
 
