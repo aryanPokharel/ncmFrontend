16b97bd76947215d1ef9ba6b1245e3e3fd1ef038
Aryan Pokharel <64863058+aryanPokharel@users.noreply.github.com>
2023-04-07 17:44:29 +0545

    Contact Page Update

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -2,7 +2,14 @@ import React from 'react'
 
 const Contact = () => {
   return (
-    <div>Contact</div>
+    <section id='contentSection'>
+
+    <div id='contactSection1'>
+          <h1>Contact Us</h1>
+          <h2>Contact for Premium Business Services</h2>
+          <h3>We always love to hear from you.</h3>
+      </div>
+    </section>
   )
 }
 
