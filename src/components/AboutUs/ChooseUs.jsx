0402d085383688741be966715d4fc443e8da85ec
Aryan Pokharel <64863058+aryanPokharel@users.noreply.github.com>
2023-04-07 11:24:00 +0545

    Design changes

--- src/components/AboutUs/ChooseUs.jsx
+++ src/components/AboutUs/ChooseUs.jsx
@@ -36,8 +36,8 @@ const ChooseUs = () => {
                 <Grid item width={"50%"}>
                     <ButtonBase sx={{ width: "100%", height: 500 }}>
                         <Img
-                        height={"100%"}
-                        width={"100%"}
+                            height={"100%"}
+                            width={"100%"}
                             alt="teamWork"
                             src="https://media.istockphoto.com/id/1075972590/vector/hand-united-together-form-lines-triangles-and-particle-style-design-illustration-vector.jpg?s=612x612&w=0&k=20&c=FhGyRn8ThLncMbogiXDRBOd72lLNb-I7gSiKE3gnPNY=" />
                     </ButtonBase>
@@ -49,12 +49,22 @@ const ChooseUs = () => {
                         alignItems="center"
                         spacing={2}
                     >
-                        <Item>Why Choose Us?</Item>
-                        <Item>Everyday, more than 400 staff driven with CAN culture are out there at your service.
-                            We believe prosperity comes from logistic, trade and ecommerce infrastructure.
-                            We are marching towards CAN VISION™ 2030 and will establish branches in each of 753 municipalities,
-                            create 10,000 new jobs and facilitate trade and commerce by connecting 90% people through our massive network.
-                            We dream big for the good of country and we have a clear plan.</Item>
+
+                        <blockquote className="blockquote">
+                            <p className="mb-0">
+                                Why Choose Us?
+                            </p>
+                        </blockquote>
+
+                        <blockquote className="blockquote">
+                            <p className="mb-0">
+                                Everyday, more than 400 staff driven with CAN culture are out there at your service.
+                                We believe prosperity comes from logistic, trade and ecommerce infrastructure.
+                                We are marching towards CAN VISION™ 2030 and will establish branches in each of 753 municipalities,
+                                create 10,000 new jobs and facilitate trade and commerce by connecting 90% people through our massive network.
+                                We dream big for the good of country and we have a clear plan.
+                            </p>
+                        </blockquote>
 
                         <AboutUsAccordion />
 
