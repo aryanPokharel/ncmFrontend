e8320eb7ac871d61471e73d012c1f7d239642cd8
Aryan Pokharel <64863058+aryanPokharel@users.noreply.github.com>
2023-04-09 17:39:42 +0545

    GetInTouchCard made responsive

--- src/components/GetInTouch/GetInTouch.jsx
+++ src/components/GetInTouch/GetInTouch.jsx
@@ -4,34 +4,18 @@ import './GetInTouch.css'
 
 const GetInTouch = () => {
     return (
-        <section className="container bcontent" id='getInTouchSection'>
-            {/* <div className="card" style={{ width: "100%" }} id='getInTouchCardSection'>
-                <div className="row no-gutters">
-                    <div className="col-sm-5" id='cardBtnSection'>
-                        <Button variant="contained" id='track-btn'>Get Started</Button>
-                    </div>
-                    <div className="col-sm-7">
-                        <div className="card-body">
-                            <h5 className="card-title">Would you like to start with us?</h5>
-                            <p className="card-text">
-                                Please contact us and one of our relationship managers will guide you with our easy onboarding process.
-                                Lets us help you meet your goals.
-                            </p>
-
-                        </div>
-                    </div>
-                </div>
-            </div> */}
+        <section id='getInTouchSection'>
+           
             <div id='getInTouchCard'>
                 <div id='getInTouchCardChild1'>
-                    <h5 className="card-title">Would you like to start with us?</h5>
-                    <p className="card-text" id='getInTouchText'>
+                    <h2 style={{color:"red"}}>Would you like to start with us?</h2>
+                    <p  id='getInTouchText'>
                         Please contact us and one of our relationship managers will guide you with our easy onboarding process.
                         Lets us help you meet your goals.
                     </p>
                 </div>
                 <div id='getInTouchCardChild2'>
-                    <Button variant="contained" id='track-btn'>Get Started</Button>
+                    <Button variant="contained" id='getInTouchBtn'>Get Started</Button>
                 </div>
             </div>
         </section>
