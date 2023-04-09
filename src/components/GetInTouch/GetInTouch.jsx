2371fc3144cb173d9c7a9f5985227dd02653abea
Aryan Pokharel <64863058+aryanPokharel@users.noreply.github.com>
2023-04-09 17:18:41 +0545

    GetInTouchUpdate

--- src/components/GetInTouch/GetInTouch.jsx
+++ src/components/GetInTouch/GetInTouch.jsx
@@ -3,9 +3,9 @@ import Button from '@mui/material/Button';
 import './GetInTouch.css'
 
 const GetInTouch = () => {
-  return (
-    <div className="container bcontent" id='getInTouchCard'>
-            <div className="card" style={{ width: "100%" }} id='getInTouchCardSection'>
+    return (
+        <section className="container bcontent" id='getInTouchSection'>
+            {/* <div className="card" style={{ width: "100%" }} id='getInTouchCardSection'>
                 <div className="row no-gutters">
                     <div className="col-sm-5" id='cardBtnSection'>
                         <Button variant="contained" id='track-btn'>Get Started</Button>
@@ -21,9 +21,21 @@ const GetInTouch = () => {
                         </div>
                     </div>
                 </div>
+            </div> */}
+            <div id='getInTouchCard'>
+                <div id='getInTouchCardChild1'>
+                    <h5 className="card-title">Would you like to start with us?</h5>
+                    <p className="card-text" id='getInTouchText'>
+                        Please contact us and one of our relationship managers will guide you with our easy onboarding process.
+                        Lets us help you meet your goals.
+                    </p>
+                </div>
+                <div id='getInTouchCardChild2'>
+                    <Button variant="contained" id='track-btn'>Get Started</Button>
+                </div>
             </div>
-        </div>
-  )
+        </section>
+    )
 }
 
 export default GetInTouch;
