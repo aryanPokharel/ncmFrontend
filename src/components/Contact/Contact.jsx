c67f28a5b8b5c709d30b7b3122297534cc26e3f9
Aryan Pokharel <64863058+aryanPokharel@users.noreply.github.com>
2023-04-10 17:15:06 +0545

    ContactForm udpate

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -22,131 +22,95 @@ const Item = styled(Paper)(({ theme }) => ({
 const Contact = () => {
   return (
     <Box sx={{ width: "100%" }}>
-      <Stack spacing={2}>
-        <Stack direction="row" justifyContent={"center"} alignItems={"center"}  id="contactStack">
+      <Stack>
+        <Stack direction="row" justifyContent={"center"} alignItems={"center"} id="contactStack">
           <Card id="contactCard">
             <CardContent id="contentHolder">
               <Typography className="cardContent" color="text.secondary" gutterBottom>
-                Contact Us
+                <h1 id="header">Contact Us</h1>
+                <hr></hr>
               </Typography>
               <Typography className="cardContent" variant="h5" component="div">
-                Contact for Premium Business Services
+                <h3 className="subHeader">Contact for Premium Business Services</h3>
               </Typography>
 
               <Typography className="cardContent" variant="body2">
-                We always love to hear from you.
+                <h3 className="subHeader">We always love to hear from you.</h3>
 
               </Typography>
             </CardContent>
 
           </Card>
         </Stack>
-        <Item>
-          <section className="text-center">
-            <h3 className="mb-5">Contact us</h3>
-            <div className="row">
-              <div className="col-lg-5">
-                <iframe
-                  src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d4201.558193865407!2d85.34623962062054!3d27.6840698307196!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x39eb19a4f4fb0179%3A0xbe53904252e95812!2sNepal%20Can%20Move!5e0!3m2!1sen!2snp!4v1681114596524!5m2!1sen!2snp"
-                  className="h-100 w-100"
-                  style={{ border: 0 }}
-                  allowFullScreen=""
-                  loading="lazy"
-                />
-              </div>
-              <div className="col-lg-7">
-                <form>
-                  <div className="row">
-                    <div className="col-md-9">
-                      <div className="row mb-4">
-                        <div className="col-md-6 mb-4 mb-md-0">
-                          <div className="form-outline">
-                            <input
-                              type="text"
-                              id="form3Example1"
-                              className="form-control"
-                            />
-                            <label
-                              className="form-label"
-                              htmlFor="form3Example1"
-                            >
-                              Full name
-                            </label>
-                          </div>
-                        </div>
-                        <div className="col-md-6">
-                          <div className="form-outline">
-                            <input
-                              type="email"
-                              id="form3Example2"
-                              className="form-control"
-                            />
-                            <label
-                              className="form-label"
-                              htmlFor="form3Example2"
-                            >
-                              Email
-                            </label>
-                          </div>
-                        </div>
-                      </div>
-                      <div className="form-outline mb-4">
-                        <input
-                          type="text"
-                          id="form3Example3"
-                          className="form-control"
-                        />
-                        <label className="form-label" htmlFor="form3Example3">
-                          Address
-                        </label>
-                      </div>
-                      <div className="form-outline mb-4">
-                        <textarea
-                          className="form-control"
-                          id="form4Example3"
-                          rows={4}
-                          defaultValue={""}
-                        />
-                        <label className="form-label" htmlFor="form4Example3">
-                          Message
-                        </label>
-                      </div>
-                      <div className="text-center text-md-start">
-                        <button
-                          type="submit"
-                          className="btn btn-primary mb-5 mb-md-0"
-                        >
-                          Send
-                        </button>
-                      </div>
-                    </div>
-                    <div className="col-md-3">
-                      <ul className="list-unstyled">
-                        <li>
-                          <i className="fas fa-map-marker-alt fa-2x text-primary" />
-                          <p>
-                            <small>New York, NY 10012, USA</small>
-                          </p>
-                        </li>
-                        <li>
-                          <i className="fas fa-phone fa-2x text-primary" />
-                          <p>
-                            <small>+ 01 234 567 89</small>
-                          </p>
-                        </li>
-                        <li>
-                          <i className="fas fa-envelope fa-2x text-primary" />
-                          <p>
-                            <small>contact@gmail.com</small>
-                          </p>
-                        </li>
-                      </ul>
-                    </div>
-                  </div>
-                </form>
-              </div>
+        <Item id="formSection">
+
+          <h3 className="mb-5">Get started - Request Free Consultation</h3>
+          <div id="formAndMap">
+            <div id="map">
+              <iframe
+                src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d4201.558193865407!2d85.34623962062054!3d27.6840698307196!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x39eb19a4f4fb0179%3A0xbe53904252e95812!2sNepal%20Can%20Move!5e0!3m2!1sen!2snp!4v1681114596524!5m2!1sen!2snp"
+                className="h-100 w-100"
+                style={{ border: 0 }}
+                allowFullScreen=""
+                loading="lazy"
+              />
             </div>
-          </section>
+            <form className="text-center border border-light p-5" id="contactForm">
+              <p className="h1 mb-4">Sign in</p>
+
+              <input
+                type="name"
+                id="defaultLoginFormEmail"
+                className="form-control mb-4"
+                placeholder="name"
+              />
+              <input
+                type="email"
+                id="defaultLoginFormEmail"
+                className="form-control mb-4"
+                placeholder="E-mail"
+              />
+              <input
+                type="phone"
+                id="defaultLoginFormEmail"
+                className="form-control mb-4"
+                placeholder="phone"
+              />
+              <input
+                type="message"
+                id="defaultLoginFormEmail"
+                className="form-control mb-4"
+                placeholder="phone"
+              />
+              <button className="btn btn-info btn-block my-4" type="submit">
+                Sign in
+              </button>
+
+            </form>
+
+
+            <div id="contactLocationSection">
+              <p>
+                <i className="fas fa-map-marker-alt fa-2x text-primary" />
+                <br></br>
+                <small>New York, NY 10012, USA</small>
+              </p>
+              <p>
+                <i className="fas fa-phone fa-2x text-primary" />
+                <br></br>
+                <small>+ 01 234 567 89</small>
+              </p>
+              <p>
+                <i className="fas fa-envelope fa-2x text-primary" />
+                <br></br>
+                <small>contact@gmail.com</small>
+              </p>
+
+
+            </div>
+          </div>
+
+
         </Item>
       </Stack>
     </Box>
