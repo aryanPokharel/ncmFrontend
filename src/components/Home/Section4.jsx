55f3f4aaf0c7ee1aea7a42643b5f3050da21ff57
Aryan Pokharel <64863058+aryanPokharel@users.noreply.github.com>
2023-04-10 09:43:17 +0545

    Update Section4.jsx

--- src/components/Home/Section4.jsx
+++ src/components/Home/Section4.jsx
@@ -15,39 +15,65 @@ const Item = styled(Paper)(({ theme }) => ({
   color: theme.palette.text.secondary,
 }));
 const Section4 = () => {
-  var [employeeState, setEmployeeState] = useState(0);
-  var [branchState, setBranchState] = useState(0);
-  var [clientState, setClientState] = useState(0);
-  var [orderState, setOrderState] = useState(0);
+  // var [employeeState, setEmployeeState] = useState(0);
+  // var [branchState, setBranchState] = useState(0);
+  // var [clientState, setClientState] = useState(0);
+  // var [orderState, setOrderState] = useState(0);
 
-  
-  const [counter, setCounter] = useState(0);
+  // useEffect(() => {
+  //   const interval = setInterval(() => {
+  //     if (employeeState < 600){
+  //       setEmployeeState(employeeState + 1);
+  //     }
+  //     if (branchState < 200){
+  //       setBranchState(branchState + 1);
+  //     }
+  //     if (clientState < 3000){
+  //       setClientState(clientState + 1);
+  //     }
+  //     if (orderState < 2){
+  //       setOrderState(orderState + 1);
+  //     }
+
+  //   }, 10);
+
+  //   return () => {
+  //     clearInterval(interval);
+  //   };
+  // });
+  const [counters, setCounters] = useState({
+    employee: 0,
+    branch: 0,
+    client: 0,
+    order: 0
+  });
+
   useEffect(() => {
     const interval = setInterval(() => {
-      if (employeeState < 600){
-        setEmployeeState(employeeState + 1);
-      }
-      if (branchState < 200){
-        setBranchState(branchState + 1);
-      }
-      if (clientState < 3000){
-        setClientState(clientState + 1);
-      }
-      if (orderState < 2){
-        setOrderState(orderState + 1);
-      }
-      
+      setCounters(prevCounters => {
+        const updatedCounters = { ...prevCounters };
+        if (updatedCounters.employee < 600) {
+          updatedCounters.employee += 1;
+        }
+        if (updatedCounters.branch < 200) {
+          updatedCounters.branch += 1;
+        }
+        if (updatedCounters.client < 3000) {
+          updatedCounters.client += 1;
+        }
+        if (updatedCounters.order < 2) {
+          updatedCounters.order += 1;
+        }
+        return updatedCounters;
+      });
     }, 10);
-
-    return () => {
-      clearInterval(interval);
-    };
-  });
+    return () => clearInterval(interval);
+  }, []);
   return (
     <div id='section4'>
       <h1>Numbers Speak!</h1>
       <h3>We have made a good start to realize our Vision 2030.</h3>
-   
+
       <Box sx={{ flexGrow: 1 }}>
         <Grid container spacing={0} sx={{ flexWrap: "nowrap", flexDirection: { xs: "column", sm: "column", md: "row" } }}>
           {Array.from(Array(4)).map((_, index) => (
@@ -55,22 +81,22 @@ const Section4 = () => {
               <Item id="section4Item">{
                 index == 0 ?
                   <div>
-                    <h2>{employeeState}+</h2>
+                    <h2>{counters.employee}+</h2>
                     <h2>Employees</h2>
                   </div>
                   :
                   (index == 1 ?
                     <div>
-                      <h2>{branchState}+</h2>
+                      <h2>{counters.branch}+</h2>
                       <h2>Branch Network</h2>
                     </div> :
                     (index == 2 ?
                       <div>
-                        <h2>{clientState}+</h2>
+                        <h2>{counters.client}+</h2>
                         <h2>Satisfied Clients</h2>
                       </div> :
                       <div>
-                        <h2>{orderState}+</h2>
+                        <h2>{counters.order}+</h2>
                         <h2>Orders Delivered</h2>
                       </div>
                     )
