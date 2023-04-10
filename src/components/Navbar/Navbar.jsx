644dfb3cebceb3654b727acc4839d21372968cd4
Aryan Pokharel <64863058+aryanPokharel@users.noreply.github.com>
2023-04-10 18:00:00 +0545

    Navbar update

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -82,42 +82,43 @@ const Navbar = () => {
 
         </div>
 
-        {!isLoggedIn ?
-          <button onClick={handleLogin} id='loginBtn'>Login</button>
-          :
-          <div className="d-flex align-items-center">
-            <div className="dropdown">
-              <a
-                className="dropdown-toggle d-flex align-items-center hidden-arrow"
-                href="#"
-                id="navbarDropdownMenuAvatar"
-                role="button"
-                data-mdb-toggle="dropdown"
-                aria-expanded="false"
-              >
-                <img
-                  src="https://mdbcdn.b-cdn.net/img/new/avatars/2.webp"
-                  className="rounded-circle"
-                  height={25}
-                  alt="Black and White Portrait of a Man"
-                  loading="lazy"
-                />
-              </a>
-              <ul
-                className="dropdown-menu dropdown-menu-end"
-                aria-labelledby="navbarDropdownMenuAvatar"
-              >
-
-                <li>
-                  <a className="dropdown-item" onClick={handleLogout}>
-                    Logout
-                  </a>
-                </li>
-              </ul>
+        <div id='navbarBtnHolder'>
+          {!isLoggedIn ?
+            <button className='navbarBtn' onClick={handleLogin} id='loginBtn'>Login</button>
+            :
+            <div className="d-flex align-items-center">
+              <div className="dropdown">
+                <a
+                  className="dropdown-toggle d-flex align-items-center hidden-arrow"
+                  href="#"
+                  id="navbarDropdownMenuAvatar"
+                  role="button"
+                  data-mdb-toggle="dropdown"
+                  aria-expanded="false"
+                >
+                  <img
+                    src="https://mdbcdn.b-cdn.net/img/new/avatars/2.webp"
+                    className="rounded-circle"
+                    height={25}
+                    alt="Black and White Portrait of a Man"
+                    loading="lazy"
+                  />
+                </a>
+                <ul
+                  className="dropdown-menu dropdown-menu-end"
+                  aria-labelledby="navbarDropdownMenuAvatar"
+                >
+                  <li>
+                    <a className="dropdown-item" onClick={handleLogout}>
+                      Logout
+                    </a>
+                  </li>
+                </ul>
+              </div>
             </div>
-          </div>
-        }
-        <button>Track</button>
+          }
+          <button className='navbarBtn' id='trackBtn'>Track</button>
+        </div>
 
       </div>
 
