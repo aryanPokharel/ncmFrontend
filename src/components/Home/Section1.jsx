2e506c376fc0c36758658775f92e927bcd5be280
Aryan Pokharel <64863058+aryanPokharel@users.noreply.github.com>
2023-04-13 10:07:22 +0545

    heading of section1 update

--- src/components/Home/Section1.jsx
+++ src/components/Home/Section1.jsx
@@ -5,13 +5,16 @@ const Section1 = () => {
   return (
     <section id='section1'>
       <div id='section1Child'>
-        <h3>Sell more online with Nepal Can Move</h3>
-        <h1>Nepal's national courier, cargo, delivery and logistics company</h1>
-        <p>Nepal Can Move, your full-service courier & logistic partner, Fast. Secure. Reliable.  You Can Count On us for Your Success and Growth. Experience service like never before.</p>
-        <Button variant="contained" id='track-btn'>GET STARTED</Button>
+        <h3 style={{ fontSize: "2vw" }}>Sell more online with Nepal Can Move</h3>
+
+        <h3 style={{ fontSize: "2vw" }}>Nepal's national courier, cargo, delivery and logistics company</h3>
+        <p style={{ fontSize: "1vw" }}>Nepal Can Move, your full-service courier & logistic partner, Fast. Secure. Reliable.  You Can Count On us for Your Success and Growth. Experience service like never before.</p>
+        <a href='/contact'>
+          <Button variant="contained" id='track-btn'>GET STARTED</Button>
+        </a>
       </div>
     </section>
-    
+
   )
 }
 
